cb08b481d5d172fe304c42072639b362ca9e5d97
stephenjjbrown <stephenjjbrown@me.com>
2019-02-22 14:51:27 -0800

    Added support for TypeScript and rollup

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const gulp = require("gulp");
 const del = require("del");
 const sass = require("gulp-sass");
+const typescript = require("gulp-typescript");
+
+const rollupPipe = require("gulp-better-rollup");
+const babel = require("rollup-plugin-babel");
+const nodeResolve = require("rollup-plugin-node-resolve");
+const commonJs = require("rollup-plugin-commonjs");
+
+
+const defaultRollupOptions = [{
+    "plugins": [
+        babel({
+            babelrc: false,
+            comments: false,
+            presets: [['@babel/preset-env', {
+                targets: {
+                    "ie": "11"
+                },
+                loose: true,
+                modules: false
+            }]],
+            runtimeHelpers: true,
+            plugins: [
+                ["@babel/transform-runtime"]
+            ]
+        }),
+        commonJs(),
+        nodeResolve()
+    ]
+}, {
+    "format": "iife"
+}]
+
 
 /**
  * Generates exports for a gulpfile based on our common business patterns
@@ -16,7 +48,6 @@ const createGulpfile = (options) => {
 
     let cleanTask = null; // Before all other build tasks
     let parallelBuildTasks = []; // One-time build of project
-    let rollupTask = null; // Compilation and bundling of JavaScript
 
     let watchTasks = []; // Watch tasks in parallel
 
@@ -45,6 +76,9 @@ const createGulpfile = (options) => {
         watchTasks = [...watchTasks, ...result.watchTasks];
     }
 
+    /**
+     * Setup Sass tasks
+     */
     if (include.scss) {
         const {src, dest, options} = include.scss;
         const {scss, scssWatch} = createSass(src, dest);
@@ -53,6 +87,50 @@ const createGulpfile = (options) => {
         parallelBuildTasks.push(scss);
         watchTasks.push(scssWatch);
     }
+    
+    /**
+     * Setup TypeScript tasks
+     */
+    if (include.ts) {
+        const {src, dest, tsconfig, clean} = include.ts;
+        const {ts, tsWatch} = createTypeScript(src, dest, tsconfig);
+
+        let rollupTask;
+        let cleanTask;
+
+        let rollupOptions = include.ts.rollup; // To deal with variable/constant scope and same-named variables
+
+        if (rollupOptions) {
+            const {src, dest, options} = rollupOptions;
+
+            const rollup = () => gulp.src(src)
+                .pipe(rollupPipe(options || defaultRollupOptions))
+                .pipe(gulp.dest(dest));
+
+            const rollupWatch = () => gulp.watch(src, rollup);
+
+            tasks.rollup = rollup;
+            rollupTask = rollup;
+            watchTasks.push(rollupWatch);
+        }
+
+        if (clean) {
+            const tsClean = () => del(clean);
+            tasks.tsClean = tsClean;
+            cleanTask = tsClean;
+        }
+
+        tsAndRollup = [ts];
+        if (rollupTask) tsAndRollup.push(rollupTask);
+        if (cleanTask) tsAndRollup.push(cleanTask);
+
+        tasks.ts = ts;
+        parallelBuildTasks.push(tsAndRollup.length > 1 ? gulp.series(...tsAndRollup) : ts); // TODO: Include Rollup and delete temp directory
+        watchTasks.push(tsWatch);
+    }
+
+
+
 
     /**
      * Setup main build and watch tasks
@@ -60,7 +138,6 @@ const createGulpfile = (options) => {
     const buildTasks = [];
     if (cleanTask) buildTasks.push(cleanTask);
     if (parallelBuildTasks.length > 0) buildTasks.push(gulp.parallel(...parallelBuildTasks));
-    if (rollupTask) buildTasks.push(rollupTask);
 
     const watch = watchTasks.length > 0 ? gulp.parallel(...watchTasks) : done => done(); // TODO: Don't even add watch if no watch tasks defined
     const build = gulp.series(...buildTasks);
@@ -132,6 +209,22 @@ const createSass = (src, dest) => {
     const scssWatch = () => gulp.watch(src, scss);
     
     return {scss, scssWatch}
+}
+
+/**
+ * Create a typescript pipe for Gulp
+ * @param {*} src 
+ * @param {*} dest 
+ * @param {*} config 
+ */
+const createTypeScript = (src, dest, config = "tsconfig.json") => {
+    const ts = () => gulp.src(src)
+        .pipe(typescript.createProject("tsconfig.json")())
+        .pipe(gulp.dest(dest));
+
+    const tsWatch = () => gulp.watch(src, ts);
+
+    return {ts, tsWatch};
 }
 
 
